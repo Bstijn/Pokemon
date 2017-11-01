d43f7c746abe08b2464561b20578e104c7741722
Laura van Helden <l.vanhelden@student.fontys.nl>
2017-11-01 10:08:39 +0100

    fix

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,80 +7,68 @@ public class Player : MonoBehaviour
 {
     private int x;
     private int y;
-    private float speed = 3.0f;
+    private float speed = 0.05f;
     private Vector3 pos;
     private Transform tr;
-    private char[,] mapTiles;
     private bool moving;
 
     void Start()
     {
         pos = transform.position;
-        tr = transform;
-
-
-        // de map moet geflipt worden, nu zit de x in de array en dat moet de y worden
-        // maar het werkt voor nu!
-
-        
-        mapTiles = new char[,] {
-            {'x','x','x','x','x','x','x' },
-            {'x','x','x','x','x','x','x' },
-            {'x','o','o','o','o','o','x' },
-            {'x','o','o','o','o','o','x' },
-            {'x','o','o','o','o','o','x' },
-            {'x','o','o','o','o','o','o' },
-            {'x','o','o','o','o','o','o' },
-            {'x','o','o','o','o','o','x' },
-            {'x','o','o','x','x','x','x' },
-            {'x','o','o','x','x','x','x' },
-            {'x','o','o','x','x','x','x' }};
-        
+        tr = transform;        
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        CheckButtonPresses();
-        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+        //CheckButtonPresses();
+        RaycastHit2D hitup = Physics2D.Raycast(transform.position, Vector2.up, 1);
+        RaycastHit2D hitdown = Physics2D.Raycast(transform.position, Vector2.down, 1);
+        RaycastHit2D hitright = Physics2D.Raycast(transform.position, Vector2.right, 1);
+        RaycastHit2D hitleft = Physics2D.Raycast(transform.position, Vector2.left, 1);
+        //==Inputs==//
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) && transform.position == pos && hitleft.collider == null)
+        {           //(-1,0)
+            pos += Vector3.left;// Add -1 to pos.x
+        }
+        if ((Input.GetKey(KeyCode.D)|| Input.GetKey(KeyCode.RightArrow)) && transform.position == pos && hitright.collider == null)
+        {           //(1,0)
+            pos += Vector3.right;// Add 1 to pos.x
+        }
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) && transform.position == pos && hitup.collider == null)
+        {           //(0,1)
+            pos += Vector3.up; // Add 1 to pos.y
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) && transform.position == pos && hitdown.collider == null)
+        {           //(0,-1)
+            pos += Vector3.down;// Add -1 to pos.y
+        }
+        //The Current Position = Move To (the current position to the new position by the speed * Time.DeltaTime)
+        transform.position = Vector3.MoveTowards(transform.position, pos, speed);
     }
 
     private void CheckButtonPresses()
     {
-        x = Convert.ToInt32(tr.position.x);
-        y = Math.Abs(Convert.ToInt32(tr.position.y));
-        if (Input.GetKey(KeyCode.RightArrow) && tr.position == pos)
-        {
-            GoToNextPosition(x+1, y, Vector3.right);
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow) && tr.position == pos)
-        {
-            GoToNextPosition(x-1, y, Vector3.left);
-        }
-        else if (Input.GetKey(KeyCode.UpArrow) && tr.position == pos)
-        {
-            GoToNextPosition(x, y - 1, Vector3.up);
-        }
-        else if (Input.GetKey(KeyCode.DownArrow) && tr.position == pos)
-        {
-            GoToNextPosition(x, y+1, Vector3.down);
-        }
-    }
+        //x = Convert.ToInt32(tr.position.x);
+        //y = Math.Abs(Convert.ToInt32(tr.position.y));
+        //if (Input.GetKey(KeyCode.RightArrow) && tr.position == pos)
+        //{
+        //    GoToNextPosition(x+1, y, Vector3.right);
+        //}
+        //else if (Input.GetKey(KeyCode.LeftArrow) && tr.position == pos)
+        //{
+        //    GoToNextPosition(x-1, y, Vector3.left);
+        //}
+        //else if (Input.GetKey(KeyCode.UpArrow) && tr.position == pos)
+        //{
+        //    GoToNextPosition(x, y - 1, Vector3.up);
+        //}
+        //else if (Input.GetKey(KeyCode.DownArrow) && tr.position == pos)
+        //{
+        //    GoToNextPosition(x, y+1, Vector3.down);
+        //}
 
-    private void GoToNextPosition(int x, int y, Vector3 vector)
-    {
-        try
-        {
-            if (mapTiles[x,y] == 'o')
-            {
-                pos += vector;
-            }
-        }
-        catch (IndexOutOfRangeException)
-        {
-            //Cannot walk there
-            Debug.Log("Player cannot walk to this position");
-        }
+        //====RayCasts====//
+        
     }
-
 }
 
