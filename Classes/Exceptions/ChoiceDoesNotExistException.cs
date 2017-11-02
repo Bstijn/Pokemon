ce642cc8515f578d22bbeede35f819eaff645f30
StijnB <s.baltessen@studen.fontys.nl>
2017-11-02 14:41:32 +0100

    functies en UnitTests van Dialogue en Choices gemaakt

---
+++ Classes/Exceptions/ChoiceDoesNotExistException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes.Exceptions
+{
+    public class ChoiceDoesNotExistException: Exception
+    {
+        public ChoiceDoesNotExistException(): base("the Choice does not exist for this dialogue or doesnt exist at all. Problem most likely in database.")
+        {
+
+        }
+    }
+}
