b818ba71b25b2262d5a9e65a36a9c9e3d531f30e
floris feddema <floris1996@hotmail.com>
2017-11-12 22:50:59 +0100

    Battle scene update
    lots of things

--- Assets/Scripts/BattleConroller.cs
+++ Assets/Scripts/BattleConroller.cs
@@ -21,6 +21,14 @@ namespace Assets.Scripts
             for (var i = 0; i < 4; i++)
             {
                 attackMenu.transform.Find("MoveButton" + i).gameObject.GetComponent<Image>().sprite = AttackTypeSprites[0];
+                //attackMenu.transform.Find("MoveButton" + i).gameObject.transform.Find("Name").gameObject.GetComponent<Text>.text = movename;
+                //attackMenu.transform.Find("MoveButton" + i).gameObject.transform.Find("PP").gameObject.GetComponent<Text>.text = ppcurrent + " / " + ppmax;
+                //if pp < 50%
+                //attackMenu.transform.Find("MoveButton" + i).gameObject.transform.Find("PP").gameObject.GetComponent<Text>().color = new Color(189f/255f, 129f/255f, 0);
+                //if pp < 25%
+                //attackMenu.transform.Find("MoveButton" + i).gameObject.transform.Find("PP").gameObject.GetComponent<Text>().color = new Color(193f/255f, 9f/255f, 0);
+                //else 
+                //attackMenu.transform.Find("MoveButton" + i).gameObject.transform.Find("PP").gameObject.GetComponent<Text>().color = new Color(0, 0, 0);
             }
         }
 
@@ -32,14 +40,31 @@ namespace Assets.Scripts
 
         public void OnUseMoveButtonPress(int moveNumber)
         {
-            
+            //do move things
         }
-
+        
         public void OnHighlightButton(int moveNumber)
         {
             InfoAttackMenu.transform.Find("DescriptionText").gameObject.GetComponent<Text>().text = Description;
             InfoAttackMenu.transform.Find("PowerText").gameObject.GetComponent<Text>().text = "Power: " + Power;
             InfoAttackMenu.transform.Find("AccuracyText").gameObject.GetComponent<Text>().text = "Accuracy: " + Accuracy;
+        }
+
+        public void OnShowPokemonMenuButtonPress(GameObject pokemonMenu)  
+        {
+            pokemonMenu.SetActive(true);
+            MainMenu.SetActive(false);
+        }
+
+        public void OnShowBackPackMenuButtonPress(GameObject backpackMenu)
+        {
+            backpackMenu.SetActive(true);
+            MainMenu.SetActive(false);
+        }
+
+        public void OnTryFleeButtonPress()
+        {
+            
         }
         #endregion
     }
