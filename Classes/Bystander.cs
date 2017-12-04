b8f12ba9849d015cebebeeda33571997d5c1d37b
Joël Emst <369152@student.fontys.nl>
2017-12-04 09:01:22 +0100

    Inventory
    Possesion

--- Classes/Bystander.cs
+++ Classes/Bystander.cs
@@ -4,7 +4,7 @@ namespace Classes
 {
     public class Bystander : Character
     {
-        public Bystander(string name, int id, string gender, int money, int posX, int posY, Location currentLocation, List<Item> inventory, List<Pokemon> pokemons) : base(name, id, gender, money, posX, posY, currentLocation, inventory, pokemons)
+        public Bystander(string name, int id, string gender, int money, int posX, int posY, Location currentLocation, List<Possesion> inventory, List<Pokemon> pokemons) : base(name, id, gender, money, posX, posY, currentLocation, inventory, pokemons)
         {
         }
     }
